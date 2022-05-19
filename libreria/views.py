78b0b14cac6f05713c6bd36792071dd6784202b5
viridianacano <imeldaviri@hotmail.com>
2022-05-18 22:55:11 -0500

    Initial commit for library system

---
+++ libreria/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def inicio(request):
+    return render (request,'paginas/inicio.html')
+
+def nosotros(request):
+    return render(request,'paginas/nosotros.html')
+
+def libros(request):
+    return render(request,'libros/index.html')
+
+def crear(request):
+    return render(request, 'libros/crear.html')
+
